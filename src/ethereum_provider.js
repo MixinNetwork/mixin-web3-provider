8034de1a4463610c75c0d2ce01c07a2c93fbf667
Crossle Song <crosslesong@gmail.com>
2024-04-14 11:58:57 +0400

    update js

--- src/ethereum_provider.js
+++ src/ethereum_provider.js
@@ -313,13 +313,7 @@ class TrustWeb3Provider extends BaseProvider {
 
     const message = typeof data === "string" ? JSON.parse(data) : data;
 
-    const { chainId } = message.domain || {};
-
-    if (!chainId || Number(chainId) !== Number(this.chainId)) {
-      throw new Error(
-        "Provided chainId does not match the currently active chain"
-      );
-    }
+    // const { chainId } = message.domain || {};
 
     const hash =
       version !== SignTypedDataVersion.V1
